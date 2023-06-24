c6fad3a64dfa95291c5fb56c292ce6d47da48a3d
abraham171996 <ibrahimmemmed3131908@gmail.com>
2023-06-24 19:56:37 +0400

    bigJS like Math methods

---
+++ app.js
@@ -0,0 +1,58 @@
+/* Class */
+
+
+// class bigJs {
+//     constructor(init) {
+//       this.init = init;
+//     }
+  
+//     add(num) {
+//       this.init += num;
+//       return this;
+//     }
+  
+//     subtract(num) {
+//       this.init -= num;
+//       return this;
+//     }
+
+//     divide(num) {
+//         this.init /= num;
+//         return this;
+//       }
+    
+//       multiply(num) {
+//         this.init *= num;
+//         return this;
+//       }
+// }
+
+
+
+// let main = new bigJs(5);
+// let result = main.add(3).subtract(4).divide(2).multiply(10)
+// console.log(result);
+
+
+/* Number */
+
+// let num = 5 
+
+// Number.prototype.add=function(num){
+//     return this + num
+// }
+
+// Number.prototype.subtract= function(num){
+//     return this - num
+// }
+
+// Number.prototype.divide = function (num) {
+//     return this / num;
+//   };
+  
+// Number.prototype.multiply = function (num) {
+//     return this * num;
+//   };
+
+// let result = (5).add(3).subtract(4).divide(2).multiply(10);
+// console.log(result);//20
